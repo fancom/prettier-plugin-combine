2a0377dfeed0bc626ed071ff90e563ea44343d92
Andrii Zakharchenko <a.zakharchenko@microsolutions.pl>
2025-09-12 13:48:52 +0200

    generic version

--- index.js
+++ index.js
@@ -1,28 +1,81 @@
-const organizeImports = require("prettier-plugin-organize-imports")
-const newLine = require("prettier-new-line-plugin")
-const allParserKeys = Array.from(
-  new Set([...Object.keys(newLine.parsers), ...Object.keys(organizeImports.parsers)])
-);
-/** @type {import("prettier").Parser}  */
-const combinedParser = {
-  ...newLine,
-  ...organizeImports,
-  parsers: allParserKeys.reduce((acc, key) => {
+const AVAILABLE_PARSERS = [
+  "prettier-plugin-organize-imports",
+  "prettier-new-line-plugin"
+];
+
+const DEFAULT_ENABLED = [...AVAILABLE_PARSERS];
+
+/**
+ * Helper to check if a parser is enabled in options.
+ */
+function isEnabled(options, parser) {
+  const enabled = options?.combineParsersEnabled || DEFAULT_ENABLED;
+  return enabled.includes(parser);
+}
+
+/**
+ * Dynamically require and collect enabled parser plugins.
+ */
+function getEnabledParsers(options) {
+  return AVAILABLE_PARSERS
+    .filter(p => isEnabled(options, p))
+    .map(p => ({
+      key: p,
+      module: require(p),
+    }));
+}
+
+function getParsers(options) {
+  const enabledParsers = getEnabledParsers(options);
+
+  // Collect all parser keys from enabled plugins
+  const allParserKeys = Array.from(
+    new Set(
+      enabledParsers.flatMap(p => Object.keys(p.module.parsers))
+    )
+  );
+
+  // Build combined parsers
+  return allParserKeys.reduce((acc, key) => {
     acc[key] = {
-      ...(newLine.parsers[key] || {}),
-      ...(organizeImports.parsers[key] || {}),
+      // Merge all parser properties from enabled plugins
+      ...enabledParsers.reduce(
+        (merged, p) => ({ ...merged, ...(p.module.parsers[key] || {}) }),
+        {}
+      ),
       preprocess(code, options) {
-        let preprocessed = code;
-        if (organizeImports.parsers[key]?.preprocess) {
-          preprocessed = organizeImports.parsers[key].preprocess(preprocessed, options);
-        }
-        if (newLine.parsers[key]?.preprocess) {
-          preprocessed = newLine.parsers[key].preprocess(preprocessed, options);
-        }
-        return preprocessed;
+        // Apply preprocess from each enabled plugin in order
+        return enabledParsers.reduce((result, p) => {
+          const parser = p.module.parsers[key];
+          if (parser?.preprocess) {
+            return parser.preprocess(result, options);
+          }
+          return result;
+        }, code);
       },
     };
     return acc;
-  }, {}),
+  }, {});
+}
+
+/** @type {import("prettier").Plugin} */
+const plugin = {
+  options: [
+    {
+      name: "parsersEnabled",
+      type: "path",
+      category: "Combine",
+      array: true,
+      default: DEFAULT_ENABLED,
+      description: "List of enabled plugins for the combine plugin.",
+    },
+  ],
+  get parsers() {
+    return getParsers(this._options || {});
+  },
+  overrideOptions(options) {
+    this._options = options;
+  },
 };
-module.exports = combinedParser;
+
+module.exports = plugin;
